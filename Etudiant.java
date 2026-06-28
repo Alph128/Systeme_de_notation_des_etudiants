cfca6c7a060e9d5f033015988160efb97b64883f
Alph128 <marcussam884@gmail.com>
2026-06-28 09:37:23 +0000

    Ajout de la classe Classe

---
+++ Etudiant.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+public class Etudiant {
+    private String nom;
+    private String prenomEtudiant;
+    private ArrayList<Double> notes;
+    public Etudiant(String nom, String prenom) {
+        this.nom = nom;
+        this.prenomEtudiant = prenom;
+        this.notes = new ArrayList<>();
+    }
+    public String getNom() {
+        return nom;
+    }
+    public String getPrenomEtudiant() {
+        return prenomEtudiant;
+    }
+    public ArrayList<Double> getNotes() {
+        return notes;
+    }
+   public void ajouterNote(double note) {
+        if (note < 0 || note > 20) {
+            throw new IllegalArgumentException("Note invalide : doit être entre 0 et 20");
+        }
+        notes.add(note);
+    }
+    public double calculerMoyenne() {
+        if (notes.isEmpty()) return 0;
+        double somme = 0;
+        for (double n : notes) somme += n;
+        return somme / notes.size();
+    }
+    @Override
+    public String toString() {
+        return "Etudiant{nom='" + nom + "', prenom='" + prenomEtudiant + "', notes=" + notes + "}";
+    }
+}
