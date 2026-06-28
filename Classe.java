655c3f070c603bec05895b93862fa998ee7c05d0
Alph128 <marcussam884@gmail.com>
2026-06-28 15:10:54 +0000

    Ajout de la classe Classe avec gestion des étudiants et méthodes associées

---
+++ Classe.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Classe {
+
+    private String nomClasse;
+    private ArrayList<Etudiant> etudiants;
+
+    public Classe(String nomClasse) {
+        this.nomClasse = nomClasse;
+        this.etudiants = new ArrayList<>();
+    }
+    
+    public void ajouterEtudiant(Etudiant etudiant) {
+        if (etudiant == null) {
+            return;
+        }
+        etudiants.add(etudiant);
+    }
+
+    public int nombreEtudiants() {
+        return etudiants.size();
+    }
+
+    @Override
+    public String toString() {
+        return "Classe{nomClasse='" + nomClasse + "', etudiants=" + etudiants + "}";
+    }
+}
